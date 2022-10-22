72625847a6a8c377b663dc39826862afa584a098
snahmAWS <113881500+snahmAWS@users.noreply.github.com>
2022-10-22 13:42:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 // Self-invoking anonymous function
 (function($) {
-    'use strict';
+	'use strict';
 
-    // Region must be defined
-    const region = 'us-east-1';
-    var readyAWS = false;
+	// Region must be defined
+	const region = 'us-east-1';
+	var readyAWS = false;
+	var tempDATA;
 
-    // aws variables
-
+	// aws variables
+	var lambda;
+	const functionName = "Snahm-Lambda-Lab"
 
 	// Show the buttons
-    $('#buttonSection').show();
+	$('#buttonSection').show();
     
 	// Click event listeners
 	$('#btnSignIn').click(function() {
         signIn();
-      });
+	});
 
 	// Click event listeners
 	$('#btnSend').click(function() {
         sendCmd();
-      });      
+	});      
     
 	/***************** The main code ******************/
 
@@ -34,21 +36,39 @@
         });
         msgOut(("Created credentials Access ID " + JSON.stringify($('#inputAccessID').val())));
         readyAWS = true; 
-    }
+	}
 
-	// Send command
-	function sendCmd(){
-        if (readyAWS){
-            msgOut("Sending command");
-            // Instantiate aws sdk service objects now that the credentials have been updated.
-                  
-        }
-    }    
+	// Send command function sendCmd(){
+	if (readyAWS){
+		msgOut("Sending command");
+		// Instantiate aws sdk service objects now that the credentials have been updated.
+		lambda = new AWS.Lambda();
+		var params = {
+			FunctionName: 'Snahm-Lambda-Lab', /* required */
+			InvocationType: 'RequestResponse',
+			LogType: 'Tail',
+			Payload: '{"key1": "Milk", "key2": "Eggs", "key3": "Bread"}'
+		};
+		lambda.invoke(params, function (err, data) {
+			if (err) {
+				console.log(err, err.stack); // an error occurred
+				msgOut('Failed. See browser console F12 for more details.');
+			}
+			else {
+				console.log(data); // successful response
+				msgOut('Invoked AWS Lambda function.');
+				msgOut('Response from Lambda function:');
+				tempData = JSON.stringify(data.Payload);
+				msgOut(tempData);
+			}
+		});
+	}
+}    
 
-	function msgOut(message){
-        $("#outputBox").append(message + "\n");
-        console.log(message);
-    }
+function msgOut(message){
+	$("#outputBox").append(message + "\n");
+	console.log(message);
+}
 
 // End 	self-invoking anonymous function
 })(jQuery);
